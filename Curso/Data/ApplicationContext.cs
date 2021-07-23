51cece899784f07da69ae736dc7367a8d58cd39f
Herbert Paes <hebert.lima@prestadores.uhgbrasil.com.br>
2021-07-23 15:34:32 -0300

    Add project files.

---
+++ Curso/Data/ApplicationContext.cs
@@ -0,0 +1,20 @@
+using CursoEFCore.Data.Configurations;
+using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoEFCore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+        }
+    }
+}
