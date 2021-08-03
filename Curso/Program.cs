5a3445979f42971572c004920102dbd3bcdca4b1
Herbert Paes <hebert.lima@prestadores.uhgbrasil.com.br>
2021-08-03 13:39:02 -0300

    Inserindo registros em massa

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -21,9 +21,59 @@ namespace CursoEFCore
             //     Console.WriteLine("Pending migration exists");
             // }
 
-            InserirDados();
+            // InserirDados();
+            InserirDadosEmMassa();
         }
 
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true,
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Herbert Paes",
+                CEP = "50290120",
+                Cidade = "Recife",
+                Estado = "PE",
+                Telefone = "8191919090"
+            };
+
+            var listaClientes = new[]
+            {
+                new Cliente
+                {
+                    Nome = "Teste 1",
+                    CEP = "50290120",
+                    Cidade = "Recife",
+                    Estado = "PE",
+                    Telefone = "8191919090"
+                },
+                new Cliente
+                {
+                    Nome = "Teste 2",
+                    CEP = "50290120",
+                    Cidade = "Recife",
+                    Estado = "PE",
+                    Telefone = "8191919090"
+                }
+            };
+
+            using var db = new Data.ApplicationContext();
+            // db.AddRange(produto, cliente);
+
+            db.Set<Cliente>().AddRange(listaClientes);
+            // db.Clientes.AddRange(listaClientes);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total registros: { registros }");
+        }
         private static void InserirDados()
         {
             var produto = new Produto
