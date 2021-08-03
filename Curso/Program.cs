1267dfe0bcdde5093444f52437e9bea1a60ebc9c
Herbert Paes <hebert.lima@prestadores.uhgbrasil.com.br>
2021-08-03 09:56:29 -0300

    Inserindo registros

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore
@@ -8,16 +10,44 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            using var db = new Data.ApplicationContext();
+            // using var db = new Data.ApplicationContext();
 
             // Indicado para utilizar somente em produção
             // db.Database.Migrate();
 
-            var migrationExists = db.Database.GetPendingMigrations().Any();
-            if (migrationExists)
+            // var migrationExists = db.Database.GetPendingMigrations().Any();
+            // if (migrationExists)
+            // {
+            //     Console.WriteLine("Pending migration exists");
+            // }
+
+            InserirDados();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
             {
-                Console.WriteLine("Pending migration exists");
-            }
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true,
+            };
+
+            using var db = new Data.ApplicationContext();
+            // Primeira opção
+            db.Produtos.Add(produto);
+            // Segunda opção
+            // db.Set<Produto>().Add(produto);
+            // Terceira opção
+            // db.Entry(produto).State = EntityState.Added;
+            // Quarta opção
+            // db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registros(s): {registros}");
+
         }
     }
 }
