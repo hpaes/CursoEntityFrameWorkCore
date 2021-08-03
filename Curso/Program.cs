0293bf3c94549fb13a9b9820a7693fc6bcc79f8c
Herbert Paes <hebert.lima@prestadores.uhgbrasil.com.br>
2021-08-02 21:48:53 -0300

    Aplicando migração

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore
 {
@@ -6,7 +7,10 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var db = new Data.ApplicationContext();
+
+            // Indicado para utilizar somente em produção
+            db.Database.Migrate();
         }
     }
 }
